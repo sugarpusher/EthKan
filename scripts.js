9d42ef75df782fd0ef4dd1d8c318e31ef45c0e5d
Barbara Gomes Carano <barbeegomes@gmail.com>
2018-09-09 15:30:27 -0400

    mobile test

--- scripts.js
+++ scripts.js
@@ -129,17 +129,17 @@ $(document).ready(function(){
   // "HOW" SECTION SLIDER INITIALIZER START
   // ======================================
 
-  // $('.slider-how').slick({
-  //   infinite: true,
-  //   slidesToScroll: 1,
-  //   slidesToShow: 1,
-  //   centerMode: false,
-  //   autoplay: false,
-  //   mobileFirst: true,
-  //   dots: true,
-  //   prevArrow: "<i class='fa fa-angle-left slick-prev' aria-hidden='true'></i>",
-  //   nextArrow: "<i class='fa fa-angle-right slick-next' aria-hidden='true'></i>"
-  // });
+  $('.slider-how').slick({
+    infinite: true,
+    slidesToScroll: 1,
+    slidesToShow: 1,
+    centerMode: false,
+    autoplay: false,
+    mobileFirst: true,
+    dots: true,
+    prevArrow: "<i class='fa fa-angle-left slick-prev' aria-hidden='true'></i>",
+    nextArrow: "<i class='fa fa-angle-right slick-next' aria-hidden='true'></i>"
+  });
 
 
 
@@ -147,52 +147,52 @@ $(document).ready(function(){
   // "WHO" SECTION SLIDER INITIALIZER START
   // ======================================
 
-  // $('.slider-who').slick({
-  //   infinite: true,
-  //   slidesToScroll: 1,
-  //   slidesToShow: 1,
-  //   centerMode: false,
-  //   autoplaySpeed: 5000,
-  //   mobileFirst: true,
-  //   prevArrow: "<i class='fa fa-angle-left slick-prev' aria-hidden='true'></i>",
-  //   nextArrow: "<i class='fa fa-angle-right slick-next' aria-hidden='true'></i>"
-  //   ,
-  //   responsive: [
-  //   {
-  //     breakpoint: 490,
-  //     settings: {
-  //       slidesToShow: 2
-  //     }
-  //   }
-  //   ,
-  //   {
-  //     breakpoint: 600,
-  //     settings: {
-  //       slidesToShow: 3
-  //     }
-  //   }
-  //   ,
-  //   {
-  //     breakpoint: 767,
-  //     settings: {
-  //       slidesToShow: 4
-  //     }
-  //   }
-  //   ,
-  //   {
-  //     breakpoint: 991,
-  //     settings: {
-  //       slidesToShow: 6
-  //     }
-  //   },
-  //   {
-  //     breakpoint: 1199,
-  //     settings: {
-  //       slidesToShow: 7
-  //     }
-  //   }
-  // ]
-  // });
+  $('.slider-who').slick({
+    infinite: true,
+    slidesToScroll: 1,
+    slidesToShow: 1,
+    centerMode: false,
+    autoplaySpeed: 5000,
+    mobileFirst: true,
+    prevArrow: "<i class='fa fa-angle-left slick-prev' aria-hidden='true'></i>",
+    nextArrow: "<i class='fa fa-angle-right slick-next' aria-hidden='true'></i>"
+    ,
+    responsive: [
+    {
+      breakpoint: 490,
+      settings: {
+        slidesToShow: 2
+      }
+    }
+    ,
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 3
+      }
+    }
+    ,
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 4
+      }
+    }
+    ,
+    {
+      breakpoint: 991,
+      settings: {
+        slidesToShow: 6
+      }
+    },
+    {
+      breakpoint: 1199,
+      settings: {
+        slidesToShow: 7
+      }
+    }
+  ]
+  });
 
 
 });
